1a8403a54caf141db2a953ea854d0628dfbad3aa
welshonionman <welshonionman@gmail.com>
2024-04-24 13:49:58 +0000

    set_seed

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,6 @@
+import os
+import numpy as np
+import random
 from pathlib import Path
 import torch
 import kornia as K
@@ -14,6 +17,17 @@ def load_torch_image(file_name: Path | str, device=torch.device("cpu")):
     """Loads an image and adds batch dimension"""
     img = K.io.load_image(file_name, K.io.ImageLoadType.RGB32, device=device)[None, ...]
     return img
+
+
+def set_seed(seed=42, cudnn_deterministic=True):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = cudnn_deterministic
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms = True
 
 
 def import_into_colmap(
