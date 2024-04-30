94532480c065f446af8963f143601dff305a3e17
welshonionman <welshonionman@gmail.com>
2024-04-30 14:47:13 +0000

    starter

---
+++ experiments/starter.py
@@ -0,0 +1,65 @@
+import torch
+import shutil
+from pathlib import Path
+import kornia as K
+from src.pipeline import run_from_config
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class Config:
+    exp_name: str = __file__.split("/")[-1].replace(".py", "")
+    is_kaggle_notebook: bool = any("KAGGLE" in item for item in dict(os.environ).keys())
+    valid_image_num: int = 3  # validationに使用する画像数（デバッグ用）
+    log_path = Path(f"/kaggle/log/{exp_name}.log")
+    gt_csv_path = Path("/kaggle/src/valid_gt.csv")
+
+    base_path: Path = Path("/kaggle/input/image-matching-challenge-2024")
+    feature_dir: Path = Path("/kaggle/.sample/")
+    target_scene: list[str] = [
+        "church",
+        "dioscuri",
+        "lizard",
+        "multi-temporal-temple-baalshamin",
+        "pond",
+        "transp_obj_glass_cup",
+        "transp_obj_glass_cylinder",
+    ]
+    device: torch.device = K.utils.get_cuda_device_if_available(0)
+
+    # get_image_pairs function's arguments
+    pair_matching_args = {
+        "model_name": "/kaggle/input/dinov2/pytorch/base/1",
+        "similarity_threshold": 0.3,
+        "tolerance": 500,
+        "min_matches": 100,
+        "exhaustive_if_less": 50,
+        "p": 2.0,
+    }
+
+    # detect_keypoints function's arguments
+    keypoint_detection_args = {
+        "num_features": 4096,
+        "resize_to": 1280,
+        "detection_threshold": 0.005,  # alikedの引数
+    }
+
+    # keypoint_distances function's arguments
+    keypoint_distances_args = {
+        "min_matches": 15,
+        "verbose": False,
+    }
+
+    # import_into_colmap function's arguments
+    colmap_mapper_options = {
+        "min_model_size": 5,  # By default colmap does not generate a reconstruction if less than 10 images are registered. Lower it to 3.
+        "max_num_models": 3,
+        "num_threads": 1,
+    }
+
+
+if __name__ == "__main__":
+    shutil.rmtree(Config.feature_dir, ignore_errors=True)
+    run_from_config(config=Config)
